2ea492847f89c09d2c30acae615d8c11ad4b6809
Barry Hayes <bhayes16@live.spcollege.edu>
2025-08-25 19:49:17 -0400

    still not able to correct the hit me button.

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -9,18 +9,18 @@ import UIKit
 import WebKit
 
 class AboutViewController: UIViewController {
-    @IBOutlet var webView: WKWebView!
-
-    @IBAction func close() {
-      dismiss(animated: true, completion: nil)
-    }
+    @IBOutlet weak var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let url = Bundle.main.url(
-            forResource: "BullsEye", withExtension: "html") {
+        
+        if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html") {
             let request = URLRequest(url: url)
             webView.load(request)
         }
+    }
+    
+    @IBAction func close() {
+      dismiss(animated: true, completion: nil)
     }
 }
--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -18,47 +18,23 @@ class ViewController: UIViewController {
     @IBOutlet var roundLabel: UILabel!
     
     override func viewDidLoad() {
-      super.viewDidLoad()
-      startNewGame()
-
-      let thumbImageNormal = UIImage(named: "SliderThumb-Normal")!
-      slider.setThumbImage(thumbImageNormal, for: .normal)
-      let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")!
-      slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
-      let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
-      let trackLeftImage = UIImage(named: "SliderTrackLeft")!
-      let trackLeftResizable = trackLeftImage.resizableImage(
-          withCapInsets: insets)
-      slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
-      let trackRightImage = UIImage(named: "SliderTrackRight")!
-      let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
-      slider.setMaximumTrackImage(trackRightResizable, for: .normal)
-    }
+        super.viewDidLoad()
+        
+        
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")!
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")!
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        let trackLeftImage = UIImage(named: "SliderTrackLeft")!
+        let trackLeftResizable = trackLeftImage.resizableImage(
+            withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        let trackRightImage = UIImage(named: "SliderTrackRight")!
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
     
-    func startNewRound() {
-        round += 1
-        targetValue = Int.random(in: 1...100)
-        currentValue = 50
-        slider.value = Float(currentValue)
-        updateLabels()
-    }
-    
-    @IBAction func startNewGame() {
-      score = 0
-      round = 0
-      startNewRound()
-      let transition = CATransition()
-      transition.type = CATransitionType.fade
-      transition.duration = 1
-      transition.timingFunction = CAMediaTimingFunction(
-        name: CAMediaTimingFunctionName.easeOut)
-      view.layer.add(transition, forKey: nil)
-    }
-    
-    func updateLabels() {
-      targetLabel.text = String(targetValue)
-      scoreLabel.text = String(score)
-      roundLabel.text = String(round)
+        startNewGame()
     }
     
     @IBAction func showAlert() {
@@ -103,5 +79,31 @@ class ViewController: UIViewController {
       currentValue = lroundf(slider.value)
     }
     
+    @IBAction func startNewGame() {
+      score = 0
+      round = 0
+      startNewRound()
+      let transition = CATransition()
+      transition.type = CATransitionType.fade
+      transition.duration = 1
+      transition.timingFunction = CAMediaTimingFunction(
+        name: CAMediaTimingFunctionName.easeOut)
+      view.layer.add(transition, forKey: nil)
+    }
+    
+    func startNewRound() {
+        round += 1
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    
+    func updateLabels() {
+      targetLabel.text = String(targetValue)
+      scoreLabel.text = String(score)
+      roundLabel.text = String(round)
+    }
 }
 
